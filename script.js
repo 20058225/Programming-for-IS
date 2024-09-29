3041e63b53026b1e4ba46097f3ed232db545e09b
20058225 <20058225@mydbs.ie>
2024-09-29 22:12:39 +0000

    Up fix

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-alert("Hello class!");
+/*If we list all the natural numbers below 10 that are multiples
+of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
 
-alert("The value of 4/3 + 1/3 + 1/3 is:  " + (4/3 + 1/3 +1/3));
-alert("The value of 1/3 + 1/3 + 4/3 is:  " + (1/3 + 1/3 + 4/3));
+1. Find the sum of all the multiples of 3 or 5 below 1000.
+
+2. Create a function to find the sum of all the multiples of a or b below n.
+(a,b,n) are function parameters
+
+3. Allow the user to enter the parameters via text box(es)
+and alert them to the output
+*/
+
+let i = 0;
+let sumNumbers = 0;
+
+let toCalc = () => {
+    let numberA = document.getElementById("inputNumberA").value;
+    let numberB = document.getElementById("inputNumberB").value;
+    let numberN = document.getElementById("inputNumberN").value;
+    for (i = 1; i < numberN; i++) {
+        if((i % numberA == 0) || (i % numberB ==0))
+            sumNumbers += i;       
+    } 
+    document.getElementById("result").innerHTML = sumNumbers;
+    alert("\n The sum of all the multiples is:  " + sumNumbers)   
+}

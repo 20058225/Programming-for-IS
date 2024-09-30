ddb1fffad1d0c0410cfa8a8f066d05e1b56671b4
20058225 <20058225@mydbs.ie>
2024-09-30 10:04:59 +0000

    update

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-/*If we list all the natural numbers below 10 that are multiples
-of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
+/*
+    If we list all the natural numbers below 10 that are multiples
+    of 3 or 5, we get 3, 5, 6 and 9.
+    The sum of these multiples is 23.
 
-1. Find the sum of all the multiples of 3 or 5 below 1000.
+    1. Find the sum of all the multiples of 3 or 5 below 1000.
 
-2. Create a function to find the sum of all the multiples of a or b below n.
-(a,b,n) are function parameters
+    2. Create a function to find the sum of all the multiples of a or b below n.
+    (a,b,n) are function parameters
 
-3. Allow the user to enter the parameters via text box(es)
-and alert them to the output
+    3. Allow the user to enter the parameters via text box(es)
+    and alert them to the output
 */
 
-let i = 0;
 let sumNumbers = 0;
 
 let toCalc = () => {
-    let numberA = document.getElementById("inputNumberA").value;
-    let numberB = document.getElementById("inputNumberB").value;
-    let numberN = document.getElementById("inputNumberN").value;
-    for (i = 1; i < numberN; i++) {
+    let numberA = document.getElementById("inputNumberA").value; //get element from HTML
+    let numberB = document.getElementById("inputNumberB").value; //get element from HTML
+    let numberN = document.getElementById("inputNumberN").value; //get element from HTML
+    for (i = 0; i < numberN; i++) {
         if((i % numberA == 0) || (i % numberB ==0))
             sumNumbers += i;       
     } 
-    document.getElementById("result").innerHTML = sumNumbers;
+    document.getElementById("result").innerHTML = sumNumbers; //update element on HTML
     alert("\n The sum of all the multiples is:  " + sumNumbers)   
 }
